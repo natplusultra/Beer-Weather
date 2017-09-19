4b1d7de99f2eb9c0a396cf99b7373d6224b04208
aschen8 <andyshoakichen@gmail.com>
2017-09-18 20:08:52 -0700

    Update crimemap.js

--- assets/javascript/crimemap.js
+++ assets/javascript/crimemap.js
@@ -4,11 +4,12 @@
 //SpotCrimeAPI
 //--will use variable array to determine data pulled from SpotCrime API
 //on buttonclick AJAX call to both GoogleMaps API and SpotCrime, issue is how to integrate
+//--use user loc data from GoogleMaps API and pullup 
+//--SpotCrime API to view crime data in area for past 1 Month max, after which the most recent 50 crime reports are displayed
 //
 //
 //GoogleMapsAPI
 //--Re GoogleMaps API, pull longitude and latitude from two inputs: user location and address input box, and then inputs them
-//--SpotCrime API to view crime data in area for past 1 Month
 //--will need to timestamp each click, and pull data from SpotCrime for that day (no need to use moment)
 //--start with a 1 square mile area
 //pull GoogleMaps API into scrolling window, be able to click on a point in scrolling window map and pull address
