fe8856aa4339e6281a80289ff92f540e94323ddc
Natalie Loman <Natalie@Natalies-MBP.local>
2017-09-19 20:23:09 -0700

    updated pseudocode

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,5 +6,6 @@
 - Using the weather, determine category of beer to pull from
 - Return 10-20 recommendations of beers
 - Use firebase to track recommended beers and save favorites
+	- Create a function for logging in and creating a user profile that will be stored in firebase so that you can save your favorites
 - Find a new library or technology to use (something for front end is probably easiest to learn)
 */
