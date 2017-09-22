b2e3fb0e89a08bdb56bc0f1447876f6ef8fab4b9
Natalie Loman <Natalie@Natalies-MBP.lan>
2017-09-22 16:24:04 -0700

    rewrote the ajax call to the weather api and updated the functionality for city and state input so that they can be used as variables in the queryURL for the ajax call

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,9 +12,15 @@ firebase.initializeApp(config);
 
 var database = firebase.database().ref("/");
 
+// creates a "location" child and a "beers" child in the main database
 var loc = database.child("location");
 var beers = database.child("beers");
 
+// initalize global variables
+var userCity;
+var userState;
+
+// code for creating a drop down form selector for the states
 var statesArray = ["AK", "AL", "AR", "AZ", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME", "MI", "MN", "MO", "MS", "MT", "NC", "ND", "NE", "NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VA", "VT", "WA", "WI", "WV", "WY"];
 
 $(document).ready(function() {
@@ -26,13 +32,33 @@ $(document).ready(function() {
 	}
 });
 
-// adding zip code for weather forecast
+// function to dynamically add a 3-day weather forecast to the DOM based on city and state input
+function createForecast() {
+	$("#weatherArea").empty(); // empties weatherArea div so that we don't get duplicates
+
+	var queryURL = "http://api.wunderground.com/api/404b6bf115d36806/forecast/q/" + userState + "/" + userCity + ".json";
+
+	// ajax call the to the Weather Underground API
+	$.ajax ({
+		url: queryURL,
+		method: "GET"
+	}).done(function(response) {
+		console.log(response);
+		var response = response.forecast.txt_forecast.forecastday;
+
+		// creates a display card for each day's forecast that contains the date/day, weather image, temperature, and weather description
+		 
+	});
+}
+
+
+// when button is clicked, createForecast function is called and data is pushed to firebase
 $("#submit-btn").on("click", function(event) {
 	event.preventDefault();
 
 	//grabs user input
-	var userCity = $("#userInput").val().trim();
-	var userState = $("#inlineFormCustomSelect").val();
+	userCity = $("#userInput").val().trim();
+	userState = $("#inlineFormCustomSelect").val();
 	console.log(userState);
 
 	//create location object
@@ -46,6 +72,14 @@ $("#submit-btn").on("click", function(event) {
 
 	// clears the input box
 	$("#userInput").val("");
+	$("#inlineFormCustomSelect").val("default");
+
+	// adds the city and state values to the html
+	$("#spanCity").html(userCity);
+	$("#spanState").html(userState);
+
+
+	createForecast();
 });
 	
 // ajax call to breweryDB api
