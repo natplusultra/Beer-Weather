65002d7850546c3eca0c1a177876e69d9c093f38
Natalie Loman <Natalie@Natalies-MBP.local>
2017-09-21 20:02:18 -0700

    updated firebase code and userinput functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,11 +10,46 @@ var config = {
 
 firebase.initializeApp(config);
 
-var database = firebase.database();
+var database = firebase.database().ref("/");
 
+var loc = database.child("location");
+var beers = database.child("beers");
+
+var statesArray = ["AK", "AL", "AR", "AZ", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME", "MI", "MN", "MO", "MS", "MT", "NC", "ND", "NE", "NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VA", "VT", "WA", "WI", "WV", "WY"];
+
+$(document).ready(function() {
+	for (var i = 0; i < statesArray.length; i++) {
+		var stateOption = $("<option>");
+		stateOption.attr("value", statesArray[i]);
+		stateOption.html(statesArray[i]);
+		$("#inlineFormCustomSelect").append(stateOption);
+	}
+});
+
+// adding zip code for weather forecast
+$("#submit-btn").on("click", function(event) {
+	event.preventDefault();
+
+	//grabs user input
+	var userCity = $("#userInput").val().trim();
+	var userState = $("#inlineFormCustomSelect").val();
+	console.log(userState);
+
+	//create location object
+	var newLocation = {
+		city: userCity,
+		state: userState
+	}
+
+	loc.push(newLocation);
+	console.log(newLocation);
+
+	// clears the input box
+	$("#userInput").val("");
+});
+	
 // ajax call to breweryDB api
 var queryURL = "http://api.brewerydb.com/v2/beers?styleId=97&key=e97ade06c4cddc2a58ecba58cb8b4bd9";
-
 
 $.ajax({
     url: queryURL,
@@ -22,3 +57,5 @@ $.ajax({
 }).done(function(response) {
 	console.log(response);
 });
+
+
